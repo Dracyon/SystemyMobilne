efc5a49e140ac92262904b0b3cad121824582231
dracyon <dracyon@gmail.com>
2020-12-07 21:04:28 +0100

    add some models

---
+++ MuscleApp/Models/Rep.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MuscleApp.Models
+{
+    public class Rep
+    {
+        public int RepNumber { get; set; }
+        public float Weight { get; set; }
+
+    }
+
+    
+}
---
+++ MuscleApp/Models/Training.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MuscleApp.Models
+{
+    public class Training
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Exercise> Exercises { get; set; }
+    }
+}
