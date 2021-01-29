39b109377e10b3fb51fbf4315c80151e9bc8cd8d
Wojciech Kowalczyk <w.kowalczyk@accenture.com>
2021-01-29 18:31:54 +0100

    new views

---
+++ iOS/ViewControllers/ExcerciseAddViewController.cs
@@ -0,0 +1,25 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+namespace MuscleApp.iOS.ViewControllers
+{
+	//[Register("ExcerciseAddViewController")]
+	public class ExcerciseAddViewController : UITableViewController
+    {
+        [Outlet]
+         UIKit.UIButton btnSave { get; set; }
+
+        void ReleaseDesignerOutlets()
+        {
+            if (btnSave != null)
+            {
+                btnSave.Dispose();
+                btnSave = null;
+            }
+        }
+    }
+}
